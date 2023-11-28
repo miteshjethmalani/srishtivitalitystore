32105ed6ea520f1d5ffd6999f6aec4692c22f144
Mitesh Jethmalani <mitesh.jethmalani@gmail.com>
2023-11-29 01:07:10 +0530

    update meta tags

--- app/constants.ts
+++ app/constants.ts
@@ -1,6 +1,6 @@
 export const APP_META_TITLE = 'Srishtivitality';
 export const APP_META_DESCRIPTION =
-  'Explore a diverse array of healing crystals available for purchase, each emanating positive vibes. Elevate your well-being by gifting these crystals to yourself or a loved one. Immerse yourself in our thoughtfully curated selection. Take the next step—shop now and welcome the harmonious energy of crystals into your life. Explore a selection of crystals such as pyrite, rose quartz, tiger eye, black tourmaline, green aventurine, citrine, selenite and many more';
+  'Discover a diverse range of healing crystals, each radiating positive energy, available for purchase. Elevate your well-being by gifting these crystals to yourself or a loved one. Immerse yourself in our carefully curated selection, featuring crystals like pyrite, rose quartz, tiger eye, black tourmaline, green aventurine, citrine, selenite, and more. Take the next step – explore, shop now, and invite the harmonious energy of crystals into your life.  ';
 export const DEMO_API_URL = 'https://api.srishtivitality.in/shop-api';
 export const APP_NUMBER_OF_PRODUCTS_LISTING = 20;
 export const ASSET_URL = 'https://api.srishtivitality.in/assets'
